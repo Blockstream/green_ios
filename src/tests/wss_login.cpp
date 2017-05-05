49967e24f28c1fc14ba7638baf01295eb80c65d5
Goncalo Carvalho <goncalo@blockstream.io>
2017-05-05 18:53:55 +0100

    clang-format for tests

--- src/tests/wss_login.cpp
+++ src/tests/wss_login.cpp
@@ -11,8 +11,10 @@ int main(int argc, char** argv)
     try {
         ga::sdk::session session;
         session.connect(DEFAULT_ENDPOINT, true);
-        session.register_user("ignore roast anger enrich income beef snap busy final dutch banner lobster bird unhappy naive spike pond industry time hero trim verb mammal asthma");
-        session.login("ignore roast anger enrich income beef snap busy final dutch banner lobster bird unhappy naive spike pond industry time hero trim verb mammal asthma");
+        session.register_user("ignore roast anger enrich income beef snap busy final dutch banner lobster bird unhappy "
+                              "naive spike pond industry time hero trim verb mammal asthma");
+        session.login("ignore roast anger enrich income beef snap busy final dutch banner lobster bird unhappy naive "
+                      "spike pond industry time hero trim verb mammal asthma");
     } catch (const std::exception& e) {
         std::cerr << "exception: " << e.what() << std::endl;
         return -1;
