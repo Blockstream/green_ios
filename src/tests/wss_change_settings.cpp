1915250df40b468a7c3b58149aab7cd2fc762970
Goncalo Carvalho <goncalo@blockstream.io>
2017-05-12 18:45:21 +0100

    add missing file

---
+++ src/tests/wss_change_settings.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "session.hpp"
+
+const std::string DEFAULT_MNEMONIC(
+    "ignore roast anger enrich income beef snap busy final dutch banner lobster bird unhappy naive "
+    "spike pond industry time hero trim verb mammal asthma");
+const std::string DEFAULT_USER_AGENT("[sw]");
+
+int main(int argc, char** argv)
+{
+    try {
+        ga::sdk::session session;
+        session.connect(ga::sdk::make_localtest_network(), true);
+        session.register_user(DEFAULT_MNEMONIC, DEFAULT_USER_AGENT);
+        session.login(DEFAULT_MNEMONIC, DEFAULT_USER_AGENT);
+        session.change_settings("tx_limits", "total", "0", "per_tx", "0", "is_fiat", "0");
+    } catch (const std::exception& e) {
+        std::cerr << "exception: " << e.what() << std::endl;
+        return -1;
+    }
+    return 0;
+}
