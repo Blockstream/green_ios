825bc5d97269053aa381359d0af3da3a91cb34f9
Goncalo Carvalho <goncalo@blockstream.io>
2017-05-14 00:22:38 +0100

    boost config for arm

--- tools/buildboost.sh
+++ tools/buildboost.sh
@@ -4,4 +4,4 @@ set -e
 cd "${MESON_SOURCE_ROOT}/thirdparty/boost_1_64_0"
 ./bootstrap.sh --prefix="${MESON_BUILD_ROOT}/thirdparty/boost_1_64_0/build"
 ./b2 -j$NUM_JOBS --with-thread --with-system install
-
+#./b2 -j$NUM_JOBS --with-thread --with-system --toolset=clang-arm --target-os=android install
