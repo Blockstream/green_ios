d47ce3e1b2e379158ca647615893c74aebf2bd04
Goncalo Carvalho <goncalo@blockstream.io>
2017-05-14 02:36:38 +0100

    more build system fixes

--- tools/buildboost.sh
+++ tools/buildboost.sh
@@ -5,6 +5,7 @@ cd "${MESON_SOURCE_ROOT}/thirdparty/boost_1_64_0"
 cp "${MESON_SOURCE_ROOT}/tools/user-config.jam" "${MESON_SOURCE_ROOT}/thirdparty/boost_1_64_0/build"
 ./bootstrap.sh --prefix="${MESON_BUILD_ROOT}/thirdparty/boost_1_64_0/build"
 if [ \( "$1" = "--arm" \) ]; then
+    export PATH=$PATH:"${MESON_SOURCE_ROOT}/toolchain/bin"
     ./b2 -j$NUM_JOBS --with-thread --with-system --toolset=clang-arm --target-os=android install
 else
     ./b2 -j$NUM_JOBS --with-thread --with-system install
