0f5917cc4d3f3381616179d542fd37edee502480
Goncalo Carvalho <goncalo@blockstream.io>
2017-05-17 22:41:35 +0100

    fix wally build

--- tools/buildwally.sh
+++ tools/buildwally.sh
@@ -17,7 +17,7 @@ if [ \( "$1" = "--arm" \) ]; then
     export CC=clang
     export AR="${MESON_BUILD_ROOT}/toolchain/bin/arm-linux-androideabi-ar"
     export RANLIB="${MESON_BUILD_ROOT}/toolchain/bin/arm-linux-androideabi-ranlib"
-    ./configure --host=$HOST_OS --enable-silent-rules --disable-dependency-tracking --target=arm --prefix="${MESON_BUILD_ROOT}/thirdparty/libwally-core/build"
+    ./configure --host=arm-linux-androideabi --build=$HOST_OS --enable-silent-rules --disable-dependency-tracking --target=arm-linux-androideabi --prefix="${MESON_BUILD_ROOT}/thirdparty/libwally-core/build"
     make -o configure clean -j$NUM_JOBS
     make -o configure -j$NUM_JOBS V=1
     make -o configure install
