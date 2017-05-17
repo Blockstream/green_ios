2b403de88a78e1112007f6019cbb3a3b2b3781ca
Goncalo Carvalho <goncalo@blockstream.io>
2017-05-17 21:56:27 +0100

    boost ndk osx changes

--- tools/buildboost.sh
+++ tools/buildboost.sh
@@ -3,6 +3,7 @@ set -e
 
 cd "${MESON_SOURCE_ROOT}/thirdparty/boost_1_64_0"
 cp "${MESON_SOURCE_ROOT}/tools/user-config.jam" "${MESON_SOURCE_ROOT}/thirdparty/boost_1_64_0"
+cp "${MESON_SOURCE_ROOT}/tools/clang.jam" "${MESON_SOURCE_ROOT}/thirdparty/boost_1_64_0/tools/build/src/tools"
 ./bootstrap.sh --prefix="${MESON_BUILD_ROOT}/thirdparty/boost_1_64_0/build"
 ./b2 --clean
 if [ \( "$1" = "--arm" \) ]; then
