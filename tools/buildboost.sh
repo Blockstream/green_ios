267a46292ca9e5862d808b32496dc1a31fa2061c
Goncalo Carvalho <goncalo@blockstream.io>
2017-05-18 21:04:26 +0100

    more boost osx cross compile fixes

--- tools/buildboost.sh
+++ tools/buildboost.sh
@@ -7,6 +7,7 @@ cp "${MESON_SOURCE_ROOT}/tools/clang.jam" "${MESON_SOURCE_ROOT}/thirdparty/boost
 ./b2 --clean
 if [ \( "$1" = "--arm" \) ]; then
     export PATH="${MESON_BUILD_ROOT}/toolchain/bin:$PATH"
+    export TARGET_OS=android
     ./b2 -j$NUM_JOBS --with-thread --with-system toolset=clang-arm target-os=android install
 else
     ./b2 -j$NUM_JOBS --with-thread --with-system install
