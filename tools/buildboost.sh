18913df7f75d203ddb7b8334e724969a3e06b7d2
Lawrence Nahum <lawrence@greenaddress.it>
2017-05-20 02:14:32 +0200

    dynamic boost

--- tools/buildboost.sh
+++ tools/buildboost.sh
@@ -1,14 +1,16 @@
 #! /usr/bin/env bash
 set -e
 cd "${MESON_SOURCE_ROOT}/thirdparty/boost_1_64_0"
-cp "${MESON_SOURCE_ROOT}/tools/user-config.jam" "${MESON_SOURCE_ROOT}/thirdparty/boost_1_64_0"
 cp "${MESON_SOURCE_ROOT}/tools/clang.jam" "${MESON_SOURCE_ROOT}/thirdparty/boost_1_64_0/tools/build/src/tools"
-./bootstrap.sh --prefix="${MESON_BUILD_ROOT}/thirdparty/boost_1_64_0/build"
-./b2 --clean
 if [ \( "$1" = "--arm" \) ]; then
     export TARGET_OS=android
     . ${MESON_SOURCE_ROOT}/tools/env.sh
+    echo "using clang : arm : arm-linux-androideabi-clang++ : --sysroot=$SYSROOT ;" > ${MESON_SOURCE_ROOT}/thirdparty/boost_1_64_0/user-config.jam
+    ./bootstrap.sh --prefix="${MESON_BUILD_ROOT}/thirdparty/boost_1_64_0/build"
+    ./b2 --clean
     ./b2 -j$NUM_JOBS --with-thread --with-system toolset=clang-arm target-os=android install
 else
+    ./bootstrap.sh --prefix="${MESON_BUILD_ROOT}/thirdparty/boost_1_64_0/build"
+    ./b2 --clean
     ./b2 -j$NUM_JOBS --with-thread --with-system install
 fi
