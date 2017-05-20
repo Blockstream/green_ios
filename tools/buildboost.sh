9265812b19429f4c36500beb96e25bc4f8cf994d
Lawrence Nahum <lawrence@greenaddress.it>
2017-05-20 04:21:07 +0200

    boostrap only system and thread

--- tools/buildboost.sh
+++ tools/buildboost.sh
@@ -8,11 +8,11 @@ if [ \( "$1" = "--ndk" \) ]; then
     export TARGET_OS=android
     . ${MESON_SOURCE_ROOT}/tools/env.sh
     echo "using clang : arm : arm-linux-androideabi-clang++ : --sysroot=$SYSROOT ;" > $boost_src_home/user-config.jam
-    ./bootstrap.sh --prefix="$boost_bld_home"
+    ./bootstrap.sh --prefix="$boost_bld_home" --with-libraries=system,thread
     ./b2 --clean
     ./b2 -j$NUM_JOBS --with-thread --with-system toolset=clang-arm target-os=android install
 else
-    ./bootstrap.sh --prefix="$boost_bld_home"
+    ./bootstrap.sh --prefix="$boost_bld_home" --with-libraries=system,thread
     ./b2 --clean
     ./b2 -j$NUM_JOBS --with-thread --with-system install
 fi
