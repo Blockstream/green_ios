4f698b950a3bb94daff849768d1e6813f0f5671d
Lawrence Nahum <lawrence@greenaddress.it>
2017-05-21 03:51:14 +0200

    quieten wget

--- tools/deps.sh
+++ tools/deps.sh
@@ -19,7 +19,7 @@ if [ ! -d "thirdparty" ]; then
 fi
 
 if [ ! -d "thirdparty/autobahn-cpp" ]; then
-    wget -O autobahn-cpp.tar.gz https://github.com/crossbario/autobahn-cpp/archive/e2d4c8186bc6f3c81f1638b07ad68fcc250c4dfb.tar.gz
+    wget -q -O autobahn-cpp.tar.gz https://github.com/crossbario/autobahn-cpp/archive/e2d4c8186bc6f3c81f1638b07ad68fcc250c4dfb.tar.gz
     echo "${SHA256SUM_AUTOBAHN}  autobahn-cpp.tar.gz" | $SHASUM --check
     tar -zxf autobahn-cpp.tar.gz -C ./thirdparty/
     mv thirdparty/*autobahn* thirdparty/autobahn-cpp
@@ -27,28 +27,28 @@ if [ ! -d "thirdparty/autobahn-cpp" ]; then
 fi
 
 if [ ! -d "thirdparty/websocketpp-0.7.0" ]; then
-    wget -O websocketpp-0.7.0.tar.gz https://github.com/zaphoyd/websocketpp/archive/0.7.0.tar.gz
+    wget -q -O websocketpp-0.7.0.tar.gz https://github.com/zaphoyd/websocketpp/archive/0.7.0.tar.gz
     echo "${SHA256SUM_WEBSOCKETPP}  websocketpp-0.7.0.tar.gz" | $SHASUM --check
     tar -zxf websocketpp-0.7.0.tar.gz -C ./thirdparty/
     rm -f websocketpp-0.7.0.tar.gz
 fi
 
 if [ ! -d "thirdparty/msgpack-2.1.1" ]; then
-    wget -O msgpack-2.1.1.tar.gz https://github.com/msgpack/msgpack-c/releases/download/cpp-2.1.1/msgpack-2.1.1.tar.gz
+    wget -q -O msgpack-2.1.1.tar.gz https://github.com/msgpack/msgpack-c/releases/download/cpp-2.1.1/msgpack-2.1.1.tar.gz
     echo "${SHA256SUM_MSGPACK}  msgpack-2.1.1.tar.gz" | $SHASUM --check
     tar -zxf msgpack-2.1.1.tar.gz -C ./thirdparty/
     rm -f msgpack-2.1.1.tar.gz
 fi
 
 if [ ! -d "thirdparty/boost_1_64_0" ]; then
-    wget -O boost-1.64.0.tar.gz https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.gz
+    wget -q -O boost-1.64.0.tar.gz https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.gz
     echo "${SHA256SUM_BOOST}  boost-1.64.0.tar.gz" | $SHASUM --check
     tar -zxf boost-1.64.0.tar.gz -C ./thirdparty/
     rm -f boost-1.64.0.tar.gz
 fi
 
 if [ ! -d "thirdparty/openssl-1.0.2k" ]; then
-    wget -O openssl-1.0.2k.tar.gz https://github.com/openssl/openssl/archive/OpenSSL_1_0_2k.tar.gz
+    wget -q -O openssl-1.0.2k.tar.gz https://github.com/openssl/openssl/archive/OpenSSL_1_0_2k.tar.gz
     echo "${SHA256SUM_OPENSSL}  openssl-1.0.2k.tar.gz" | $SHASUM --check
     tar -zxf openssl-1.0.2k.tar.gz -C ./thirdparty/
     mv thirdparty/openssl* thirdparty/openssl-1.0.2k
@@ -56,7 +56,7 @@ if [ ! -d "thirdparty/openssl-1.0.2k" ]; then
 fi
 
 if [ ! -d "src/wally" ]; then
-    wget -O wallycore.tar.gz https://github.com/jgriffiths/libwally-core/archive/08caa2c924a796f0ed53e3d4332889d4808acd33.tar.gz
+    wget -q -O wallycore.tar.gz https://github.com/jgriffiths/libwally-core/archive/08caa2c924a796f0ed53e3d4332889d4808acd33.tar.gz
     echo "${SHA256SUM_WALLYCORE}  wallycore.tar.gz" | $SHASUM --check
     tar -zxf wallycore.tar.gz -C ./src/
     mv src/libwally-core-08caa2c924a796f0ed53e3d4332889d4808acd33 src/wally
