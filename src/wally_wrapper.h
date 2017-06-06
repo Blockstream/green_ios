2e6a066d2c729fcda4ead42047cdde3950e37dad
Goncalo Carvalho <goncalo@blockstream.io>
2017-06-06 19:04:48 +0100

    add wally swift

---
+++ src/wally_wrapper.h
@@ -0,0 +1,10 @@
+#ifndef GA_SDK_AUTOBAHN_WRAPPER_HPP
+#define GA_SDK_AUTOBAHN_WRAPPER_HPP
+#pragma once
+
+#include <wally_bip32.h>
+#include <wally_bip39.h>
+#include <wally_core.h>
+#include <wally_crypto.h>
+
+#endif
