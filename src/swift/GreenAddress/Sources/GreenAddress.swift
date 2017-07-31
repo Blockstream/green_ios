d4764b51f8b0126512bae6108dbf6332fca5bfe9
Goncalo Carvalho <goncalo@blockstream.io>
2017-07-31 19:52:22 +0100

    allow to distinguish between in/out txs (simplified for greenbits-ios)

--- src/swift/GreenAddress/Sources/GreenAddress.swift
+++ src/swift/GreenAddress/Sources/GreenAddress.swift
@@ -12,11 +12,80 @@ public enum Network: Int32 {
     case TestNet = 2
 }
 
+public struct Transaction {
+    public enum TransactionType {
+        case Out
+        case In
+        case Recovery
+    }
+
+    let amount: Int64
+    let type: TransactionType
+    let tx: [String: Any]
+
+    public init(tx: [String: Any]) {
+        self.tx = tx;
+
+        var amount: Int64 = 0
+
+        let epsList : [[String: Any]] = tx["eps"] as! [[String: Any]]
+        for eps in epsList {
+            let isCredit = eps["is_credit"] as! Bool
+            let isRelevant = eps["is_relevant"] as! Bool
+
+            if (!isRelevant) {
+                continue;
+            }
+
+            if let value_str = eps["value"] {
+
+                let value = Int64(value_str as! String)
+
+                if (!isCredit) {
+                    amount -= value!
+                }
+                else {
+                    amount += value!
+                }
+            }
+        }
+
+        if amount >= 0 {
+            self.type = TransactionType.In
+        }
+        else {
+            self.type = TransactionType.Out
+        }
+
+        self.amount = amount
+    }
+
+    func getAs<T>(key: String) -> T {
+        return tx[key] as! T
+    }
+
+    public func getFee() -> Int64 {
+        return getAs(key: "fee")
+    }
+
+    public func isInstant() -> Bool {
+        return getAs(key: "instant")
+    }
+
+    public func getType() -> TransactionType {
+        return self.type
+    }
+
+    public func getAmount() -> Int64 {
+        return self.amount
+    }
+}
+
 public class Session {
     var session: OpaquePointer? = nil
 
     public init() throws {
-        if GA_create_session(&session) == GA_ERROR {
+        guard GA_create_session(&session) == GA_OK else {
             throw GaError.GenericError
         }
     }
@@ -26,26 +95,36 @@ public class Session {
     }
 
     public func connect(network: Network, debug: Bool = false) throws {
-        if GA_connect(session, network.rawValue, debug ? GA_TRUE : GA_FALSE) == GA_ERROR {
+        guard GA_connect(session, network.rawValue, debug ? GA_TRUE : GA_FALSE) == GA_OK else {
             throw GaError.GenericError
         }
     }
 
     public func registerUser(mnemonic: String) throws {
-        if GA_register_user(session, mnemonic) == GA_ERROR {
+        guard GA_register_user(session, mnemonic) == GA_OK else {
             throw GaError.GenericError
         }
     }
 
     public func login(mnemonic: String) throws {
-        if GA_login(session, mnemonic) == GA_ERROR {
+        guard GA_login(session, mnemonic) == GA_OK else {
             throw GaError.GenericError
         }
     }
 
-    public func getTxList(begin: Date, end: Date, subaccount: Int) throws -> [String: Any]? {
+    func transactionsFromDict(txList: [[String: Any]]) -> [Transaction] {
+        var txs = [Transaction]()
+        for tx in txList {
+            txs.append(Transaction(tx: tx))
+        }
+        return txs
+    }
+
+    public func getTxList(begin: Date, end: Date, subaccount: Int) throws -> [Transaction]? {
         var txs: OpaquePointer? = nil
-        if GA_get_tx_list(session, Int(begin.timeIntervalSince1970), Int(end.timeIntervalSince1970), subaccount, 0, 0, String(), &txs) == GA_ERROR {
+        let startDate = Int(begin.timeIntervalSince1970)
+        let endDate = Int(end.timeIntervalSince1970)
+        guard GA_get_tx_list(session, startDate, endDate, subaccount, 0, 0, String(), &txs) == GA_OK else {
             throw GaError.GenericError
         }
         defer {
@@ -53,14 +132,14 @@ public class Session {
         }
 
         var bytes : UnsafeMutablePointer<Int8>? = nil
-        if GA_convert_tx_list_to_json(txs, &bytes) == GA_ERROR {
+        guard GA_convert_tx_list_to_json(txs, &bytes) == GA_OK else {
             throw GaError.GenericError
         }
         defer {
             GA_destroy_string(bytes)
         }
 
-        var dict: [String: Any]? = nil;
+        var dict: [String: Any]? = nil
 
         let json: String = String(cString: bytes!)
         if let data = json.data(using: .utf8) {
@@ -71,12 +150,17 @@ public class Session {
             }
         }
 
-        return dict
+        if let list = dict?["list"] {
+            return transactionsFromDict(txList: list as! [[String: Any]])
+        }
+        else {
+            return nil;
+        }
     }
 
     public func getReceiveAddress() -> String? {
         var bytes : UnsafeMutablePointer<Int8>? = nil
-        if GA_get_receive_address(session, GA_ADDRESS_TYPE_P2SH, 0, &bytes) == GA_ERROR {
+        guard GA_get_receive_address(session, GA_ADDRESS_TYPE_P2SH, 0, &bytes) == GA_OK else {
             return nil 
         }
         defer {
@@ -88,7 +172,7 @@ public class Session {
 
 public func generateMnemonic(lang: String) throws -> String {
     var bytes : UnsafeMutablePointer<Int8>?
-    if GA_generate_mnemonic(lang, &bytes) == GA_ERROR {
+    guard GA_generate_mnemonic(lang, &bytes) == GA_OK else {
         throw GaError.GenericError
     }
     defer {
