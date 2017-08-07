c2ca0179f418a24e8ea00ee1c66ea5b9113574d8
Goncalo Carvalho <goncalo@blockstream.io>
2017-08-07 11:17:04 +0100

    update to wally 0.4.0

--- tools/buildwallycore.sh
+++ tools/buildwallycore.sh
@@ -27,7 +27,7 @@ if [ \( "$1" = "--ndk" \) ]; then
     export CFLAGS="$SDK_CFLAGS -fPIC"
 
     android_build_wally $HOST_ARCH "${MESON_BUILD_ROOT}/toolchain" $ANDROID_VERSION --host=$SDK_PLATFORM --build=$HOST_OS \
-          --enable-static --disable-shared --$ENABLE_SWIG_JAVA --disable-dependency-tracking --target=$SDK_PLATFORM --prefix="${MESON_BUILD_ROOT}/libwally-core/build"
+          --enable-static --disable-shared --$ENABLE_SWIG_JAVA --target=$SDK_PLATFORM --prefix="${MESON_BUILD_ROOT}/libwally-core/build"
 
     make -o configure install
 elif [ \( "$1" = "--iphone" \) -o \( "$1" = "--iphonesim" \) ]; then
@@ -35,18 +35,13 @@ elif [ \( "$1" = "--iphone" \) -o \( "$1" = "--iphonesim" \) ]; then
     export LDFLAGS="$SDK_LDFLAGS -isysroot ${IOS_SDK_PATH} -miphoneos-version-min=9.0"
     export CC=${XCODE_DEFAULT_PATH}/clang
     export CXX=${XCODE_DEFAULT_PATH}/clang++
-    export AR_FLAGS="-static -o"
     ./configure --host=armv7-apple-darwin --with-sysroot=${IOS_SDK_PATH} --build=$HOST_OS \
-                --enable-static --disable-shared --disable-dependency-tracking --prefix="${MESON_BUILD_ROOT}/libwally-core/build"
+                --enable-static --disable-shared --prefix="${MESON_BUILD_ROOT}/libwally-core/build"
     make -o configure clean -j$NUM_JOBS
     make -o configure -j$NUM_JOBS
     make -o configure install
 else
-    ./configure --$ENABLE_SWIG_JAVA --host=$HOST_OS -enable-static --disable-shared --disable-dependency-tracking --prefix="${MESON_BUILD_ROOT}/libwally-core/build"
-
-    if [ "$(uname)" == "Darwin" ]; then
-        patch -p0 < ${MESON_SOURCE_ROOT}/tools/libtool.patch
-    fi
+    ./configure --$ENABLE_SWIG_JAVA --host=$HOST_OS -enable-static --disable-shared --prefix="${MESON_BUILD_ROOT}/libwally-core/build"
 
     make -j$NUM_JOBS
     make install
--- tools/deps.sh
+++ tools/deps.sh
@@ -4,7 +4,7 @@ set -e
 SHA256SUM_AUTOBAHN=612e0f9fea274ee7a2b3873c7ab86ba82c5ed24a4aa4f125cdeb155c21656dca
 SHA256SUM_WEBSOCKETPP=07b3364ad30cda022d91759d4b83ff902e1ebadb796969e58b59caa535a03923
 SHA256SUM_MSGPACK=beaac1209f33276b5a75e7a02f8689ed44b97209cef82ba0909e06f0c45f6cae
-SHA256SUM_WALLYCORE=9bc03a18405db5bfc779021ec6b76e7d231b61fa51f96e586b290c4f38d0431b
+SHA256SUM_WALLYCORE=977e2f8be22755d1ff9329ce948974c7624dbf3375d451552cf31d2ffb93dd14
 SHA256SUM_BOOST=0445c22a5ef3bd69f5dfb48354978421a85ab395254a26b1ffb0aa1bfd63a108
 SHA256SUM_OPENSSL=a3d3a7c03c90ba370405b2d12791598addfcafb1a77ef483c02a317a56c08485
 
@@ -39,7 +39,7 @@ prepare_pkg websocketpp-0.7.0 https://github.com/zaphoyd/websocketpp/archive/0.7
 prepare_pkg msgpack-2.1.3 https://github.com/msgpack/msgpack-c/releases/download/cpp-2.1.3/msgpack-2.1.3.tar.gz ${SHA256SUM_MSGPACK}
 prepare_pkg boost_1_64_0 https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.gz ${SHA256SUM_BOOST}
 prepare_pkg openssl-1.0.2l https://github.com/openssl/openssl/archive/OpenSSL_1_0_2l.tar.gz ${SHA256SUM_OPENSSL}
-prepare_pkg libwally-core https://github.com/jgriffiths/libwally-core/archive/42b91ecff6f0c84f8f97e7f54e5ff4768790a2cf.tar.gz ${SHA256SUM_WALLYCORE}
+prepare_pkg libwally-core https://github.com/ElementsProject/libwally-core/archive/release_0.4.0.tar.gz ${SHA256SUM_WALLYCORE}
 
 function move_if() {
     if [ ! -d "$DEPS_BLD_DIR/$2" ]; then
@@ -49,4 +49,4 @@ function move_if() {
 move_if *autobahn* autobahn-cpp
 move_if openssl* openssl-1.0.2l
 move_if *msgpack* msgpack
-move_if libwally-core-42b91ecff6f0c84f8f97e7f54e5ff4768790a2cf* libwally-core
+move_if libwally-core-* libwally-core
