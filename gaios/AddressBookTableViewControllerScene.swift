2fd7e3bf8a4ccb596399106dfed308d5cd269b80
Goncalo Carvalho <goncalo@blockstream.io>
2017-11-17 14:18:15 +0000

    more view model

--- gaios/AddressBookTableViewControllerScene.swift
+++ gaios/AddressBookTableViewControllerScene.swift
@@ -5,33 +5,52 @@
 
 import UIKit
 
+class AddressBookItem {
+
+}
+
+class AddressBookTableViewModel: NSObject {
+    var items = [AddressBookItem]()
+
+    override init() {
+        super.init()
+    }
+}
+
+extension AddressBookTableViewModel: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Address Book"
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AddressBookTableCell", for: indexPath)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    }
+}
+
 class AddressBookTableViewControllerScene: UITableViewController {
+    fileprivate let viewModel = AddressBookTableViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.dataSource = viewModel
+
+        tableView.tableFooterView = UIView()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-    }
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Address Book"
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "AddressBookTableCell", for: indexPath)
-
-        return cell
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
     }
 }
