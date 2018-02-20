b39c1b1056e7945aca6531b08349d7d4d9afc0fc
Goncalo Carvalho <goncalo@blockstream.io>
2018-02-20 23:18:19 +0000

    support multiple analyzers

--- tools/build.sh
+++ tools/build.sh
@@ -17,16 +17,16 @@ if [ "$(uname)" == "Darwin" ]; then
     GETOPT='/usr/local/opt/gnu-getopt/bin/getopt'
 fi
 
-TEMPOPT=`"$GETOPT" -n "'build.sh" -o z,x,c,b: -l analyze,address_sanitizer,clang,gcc,compiler_version:,ndk:,iphone:,iphonesim:,buildtype: -- "$@"`
+TEMPOPT=`"$GETOPT" -n "'build.sh" -o x,b: -l analyze,clang,gcc,sanitizer:,compiler-version:,ndk:,iphone:,iphonesim:,buildtype: -- "$@"`
 eval set -- "$TEMPOPT"
 while true; do
     case "$1" in
-        -z | --address_sanitizer ) MESON_OPTIONS="$MESON_OPTIONS -Db_sanitize=address"; shift ;;
         -x | --analyze ) ANALYZE=true; shift ;;
         -b | --buildtype ) MESON_OPTIONS="$MESON_OPTIONS --buildtype=$2"; shift 2 ;;
+        --sanitizer ) MESON_OPTIONS="$MESON_OPTIONS -Db_sanitize=$2"; shift 2 ;;
         --clang | --gcc | --ndk ) break ;;
         --iphone | --iphonesim ) LIBTYPE="$2"; break ;;
-        --compiler_version) COMPILER_VERSION="-$2"; shift 2 ;;
+        --compiler-version) COMPILER_VERSION="-$2"; shift 2 ;;
         -- ) shift; break ;;
         *) break ;;
     esac
