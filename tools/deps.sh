9a7c648d131bc07d800ed90093baa617a865e9eb
Goncalo Carvalho <goncalo@blockstream.io>
2018-02-21 12:27:23 +0000

    upgrade_deps + increase tests timeout

--- tools/deps.sh
+++ tools/deps.sh
@@ -3,7 +3,7 @@ set -e
 
 SHA256SUM_AUTOBAHN=612e0f9fea274ee7a2b3873c7ab86ba82c5ed24a4aa4f125cdeb155c21656dca
 SHA256SUM_WEBSOCKETPP=07b3364ad30cda022d91759d4b83ff902e1ebadb796969e58b59caa535a03923
-SHA256SUM_MSGPACK=beaac1209f33276b5a75e7a02f8689ed44b97209cef82ba0909e06f0c45f6cae
+SHA256SUM_MSGPACK=6126375af9b204611b9d9f154929f4f747e4599e6ae8443b337915dcf2899d2b
 SHA256SUM_WALLYCORE=4df42cbc360813dedb4b3727bbee662aef5cc7e4120783380a91024c4f06abe6
 SHA256SUM_BOOST=0445c22a5ef3bd69f5dfb48354978421a85ab395254a26b1ffb0aa1bfd63a108
 SHA256SUM_OPENSSL=9066c68c1aa8e8719af61cb82b88156ab07b3ad2a9ab1f874a8afb324583b1b6
@@ -36,7 +36,7 @@ function prepare_pkg() {
 
 prepare_pkg autobahn-cpp https://github.com/crossbario/autobahn-cpp/archive/e2d4c8186bc6f3c81f1638b07ad68fcc250c4dfb.tar.gz ${SHA256SUM_AUTOBAHN}
 prepare_pkg websocketpp-0.7.0 https://github.com/zaphoyd/websocketpp/archive/0.7.0.tar.gz ${SHA256SUM_WEBSOCKETPP}
-prepare_pkg msgpack-2.1.3 https://github.com/msgpack/msgpack-c/releases/download/cpp-2.1.3/msgpack-2.1.3.tar.gz ${SHA256SUM_MSGPACK}
+prepare_pkg msgpack-2.1.5 https://github.com/msgpack/msgpack-c/releases/download/cpp-2.1.5/msgpack-2.1.5.tar.gz ${SHA256SUM_MSGPACK}
 prepare_pkg boost_1_64_0 https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.gz ${SHA256SUM_BOOST}
 prepare_pkg openssl-1.0.2m https://github.com/openssl/openssl/archive/OpenSSL_1_0_2m.tar.gz ${SHA256SUM_OPENSSL}
 prepare_pkg libwally-core https://github.com/jgriffiths/libwally-core/archive/txs.tar.gz ${SHA256SUM_WALLYCORE}
