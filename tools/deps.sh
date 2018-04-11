479c947c516732ba485c1c0c79c55045251d320d
Jon Griffiths <jon_p_griffiths@yahoo.com>
2018-04-11 12:13:47 +1200

    Move to wally 0.6.0 current master on ElementsProject

--- tools/deps.sh
+++ tools/deps.sh
@@ -4,7 +4,7 @@ set -e
 SHA256SUM_AUTOBAHN=41733e7c8287c31126e973bb1056a83236cd5bbb9507053a5a84e78f57791b90
 SHA256SUM_WEBSOCKETPP=07b3364ad30cda022d91759d4b83ff902e1ebadb796969e58b59caa535a03923
 SHA256SUM_MSGPACK=6126375af9b204611b9d9f154929f4f747e4599e6ae8443b337915dcf2899d2b
-SHA256SUM_WALLYCORE=f6101684eb49339c813d649366c8941be9da56ce0ba772f2d8fed3761fe16057
+SHA256SUM_WALLYCORE=739def94aa2733d533d3ff0e4e9e6b9fc1ebf1c4fc07bbfbb9dc7c34ca1f41ea
 SHA256SUM_BOOST=bd0df411efd9a585e5a2212275f8762079fed8842264954675a4fddc46cfcf60
 SHA256SUM_OPENSSL=fd8bff81636c262ff82cb22286957d73213f899c1a80a3ec712c7ae80761ea9b
 
@@ -39,7 +39,7 @@ prepare_pkg websocketpp-0.7.0 https://github.com/zaphoyd/websocketpp/archive/0.7
 prepare_pkg msgpack-2.1.5 https://github.com/msgpack/msgpack-c/releases/download/cpp-2.1.5/msgpack-2.1.5.tar.gz ${SHA256SUM_MSGPACK}
 prepare_pkg boost_1_66_0 https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz ${SHA256SUM_BOOST}
 prepare_pkg openssl-1.0.2o https://github.com/openssl/openssl/archive/OpenSSL_1_0_2o.tar.gz ${SHA256SUM_OPENSSL}
-prepare_pkg libwally-core https://github.com/jgriffiths/libwally-core/archive/txs.tar.gz ${SHA256SUM_WALLYCORE}
+prepare_pkg libwally-core https://github.com/ElementsProject/libwally-core/archive/4e41b44c55e40a098be163962fe6fc77698094e2.tar.gz ${SHA256SUM_WALLYCORE}
 
 function move_if() {
     if [ ! -d "$DEPS_BLD_DIR/$2" ]; then
