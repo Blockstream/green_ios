2000a406569ca22483eea7c3520e4c42acb25a79
Goncalo Carvalho <goncalo@blockstream.io>
2018-04-18 22:48:51 +0100

    update clean.sh until meson bug gets fixed

--- tools/clean.sh
+++ tools/clean.sh
@@ -2,3 +2,8 @@
 set -e
 
 rm -fr build-*
+rm -fr subprojects/boost_1_66_0
+rm -fr subprojects/libwally-core-987575025520d18bac31e6e2d27c8c936d812c64
+rm -fr subprojects/autobahn-cpp-cd74d3ccd7b600e4fef6e1c4cbe584ffd91048f5
+rm -fr subprojects/openssl-OpenSSL_1_0_2o
+rm -fr subprojects/packagecache
