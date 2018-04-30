a2c1abe425449a8e71357f9ccd18763a6432aa2c
Goncalo Carvalho <goncalo@blockstream.io>
2018-04-30 17:42:54 +0100

    update clean script

--- tools/clean.sh
+++ tools/clean.sh
@@ -4,7 +4,7 @@ set -e
 rm -fr build-*
 rm -fr subprojects/boost_1_66_0
 rm -fr subprojects/libwally-core-987575025520d18bac31e6e2d27c8c936d812c64
-rm -fr subprojects/autobahn-cpp-cd74d3ccd7b600e4fef6e1c4cbe584ffd91048f5
+rm -fr subprojects/autobahn-cpp-a4deb12964fe1739144f2ea37ba4241a779344b7
 rm -fr subprojects/webscocketpp-294cc0d42957078176ff8f39397862e089838543
 rm -fr subprojects/openssl-OpenSSL_1_0_2o
 rm -fr subprojects/packagecache
