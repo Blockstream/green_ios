d23798b24eb3d0990c9b5e0ef5592f0faac21c9b
Yostra <straja.mark@gmail.com>
2018-06-27 17:57:50 -0700

    uicolor and view extenstion

---
+++ gaios/helpers/UIColor+Hex.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Hex.swift
+//  gaios
+//
+//  Created by Strahinja Markovic on 6/11/18.
+//  Copyright © 2018 Goncalo Carvalho. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    class func customLightGreen() -> UIColor {
+        return UIColor(red:89.0/255.0, green:173.0/255.0, blue:87.0/255.0, alpha:1);
+    }
+    class func customLightGray() -> UIColor {
+        return UIColor(red:137.0/255.0, green:137.0/255.0, blue:137.0/255.0, alpha:1);
+    }
+}
