29a22fc297388f8969887b8ad091e97f9b50c9b3
Yostra <straja.mark@gmail.com>
2018-07-02 20:29:51 -0700

    remove gdk'

--- gdk/src/swift/GreenAddress/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "GreenAddress",
-    dependencies: [
-        .Package(url: "https://github.com/mxcl/PromiseKit", majorVersion: 6)
-    ]
-)
