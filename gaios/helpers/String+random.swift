135c2231cc0186c0e9537f9a2ea3b88a0f07da39
Yostra <straja.mark@gmail.com>
2018-07-09 16:04:56 -0700

    format satoshi value to btc

--- gaios/helpers/String+random.swift
+++ gaios/helpers/String+random.swift
@@ -9,19 +9,26 @@
 import Foundation
 
 extension String {
-    
+
     static var chars: [Character] = {
         return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".map({$0})
     }()
-    
+
     static func random(length: Int) -> String {
         var partial: [Character] = []
-        
+
         for _ in 0..<length {
             let rand = Int(arc4random_uniform(UInt32(chars.count)))
             partial.append(chars[rand])
         }
-        
+
         return String(partial)
+    }
+
+    static func satoshiToBTC(satoshi: String) -> String {
+        let satoshi: Double = Double(satoshi)!
+        let btc = satoshi / 100000000
+        let result: String = String(format: "%g BTC", btc)
+        return result
     }
 }
