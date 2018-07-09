741e84e707c822948a9d4f269d8bcf71373e96b9
Yostra <straja.mark@gmail.com>
2018-07-09 15:58:28 -0700

    colored qr generator

---
+++ gaios/helpers/QRImageGenerator.swift
@@ -0,0 +1,36 @@
+//
+//  QRImageGenerator.swift
+//  gaios
+//
+//  Created by Strahinja Markovic on 7/4/18.
+//  Copyright © 2018 Goncalo Carvalho. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QRImageGenerator {
+    
+    static func imageForAddress(address: String, frame: CGRect) -> UIImage? {
+        let data = address.data(using: String.Encoding.ascii, allowLossyConversion: false)
+        
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        guard let colorFilter = CIFilter(name: "CIFalseColor") else { return nil }
+        filter!.setValue(data, forKey: "inputMessage")
+        filter!.setValue("Q", forKey: "inputCorrectionLevel")
+        colorFilter.setValue(filter!.outputImage, forKey: "inputImage")
+        colorFilter.setValue(CIColor(color: UIColor.customQRColorGreen()), forKey: "inputColor1") // Background white
+        colorFilter.setValue(CIColor(color: UIColor.customQRColorDark()), forKey: "inputColor0")
+        
+        
+        guard let qrCodeImage = colorFilter.outputImage
+            else {
+                return nil
+        }
+        let scaleX = frame.size.width / qrCodeImage.extent.size.width
+        let scaleY = frame.size.height / qrCodeImage.extent.size.height
+        let scaledImage = qrCodeImage.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
+        
+        return UIImage(ciImage: scaledImage)
+    }
+}
