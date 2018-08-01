8955f608bdc019863e9caeb8eb920b6cd26c8423
Yostra <straja.mark@gmail.com>
2018-08-01 10:45:55 -0700

    settings ui

--- gaios/SettingsViewController.swift
+++ gaios/SettingsViewController.swift
@@ -13,18 +13,63 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     
     @IBOutlet weak var tableView: UITableView!
     
-    let settings:[String] = ["Currency", "Notifications", "nLockTime", "Two Factor Authentication", "Security", "Remove Account"]
-    
+    let settingTitles:[String] = ["Account", "Security", "Advanced", "About"]
+    let sectionAccount:[String] = ["Alternative currency", "Show Bitcoin in", "Watch-only Login"]
+    let sectionSecurity:[String] = ["Show Recovery Seed", "Screen Lock", "Two-factor Authentication", "Support"]
+    let sectionAdvanced:[String] = ["Enable Segwit", "nLockTimeTransactions", "SPV Synchronization"]
+    let sectionAbout:[String] = ["Version", "Terms of use", "Privacy Policy"]
+    let settingsIcon:[UIImage] = [#imageLiteral(resourceName: "account"),#imageLiteral(resourceName: "security"),#imageLiteral(resourceName: "advanced"),#imageLiteral(resourceName: "about")]
+
+    lazy var allSettings:[[String]] = [sectionAccount, sectionSecurity, sectionAdvanced, sectionAbout]
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return  settings.count
+        let settingsGroup = allSettings[section]
+        return  settingsGroup.count
     }
-    
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return  allSettings.count
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath) as! SettingsCell
-        cell.label.text = settings[indexPath.row]
+        let settingsGroup = allSettings[indexPath.section]
+        cell.label.text = settingsGroup[indexPath.row]
+        cell.separatorInset = UIEdgeInsetsMake(0, 16, 0, 10)
         return cell
     }
-    
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 45
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView()
+        header.backgroundColor = UIColor.customTitaniumDark()
+        let imageView = UIImageView()
+        imageView.frame = CGRect(x: 0, y: 0, width: 21, height: 21)
+        imageView.image = settingsIcon[section]
+        header.insertSubview(imageView, at: 0)
+        let title = UILabel()
+        title.text = settingTitles[section]
+        title.frame = CGRect(x: 0, y: 0, width: 21, height: 41)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.textColor = UIColor.customTitaniumLight()
+        header.insertSubview(title, at: 1)
+        //constrain <-16[image]->16 [Title]
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.height, multiplier: 1, constant: 21).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.width, multiplier: 1, constant: 21).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: header, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: 16).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: header, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0).isActive = true
+
+        NSLayoutConstraint(item: title, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: header, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: title, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: imageView, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: 5).isActive = true
+
+        return header
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
