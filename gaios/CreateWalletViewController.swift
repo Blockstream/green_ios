91c1cd07f440af0ad011842cb6641248efcb9f33
Yostra <straja.mark@gmail.com>
2018-08-12 23:14:21 -0700

    create step indicator programatically instead of using png/jpg

--- gaios/CreateWalletViewController.swift
+++ gaios/CreateWalletViewController.swift
@@ -23,7 +23,11 @@ class CreateWalletViewController: UIViewController {
     @IBOutlet weak var word5: UILabel!
     @IBOutlet weak var word6: UILabel!
     lazy var arrayLabels: [UILabel] = [self.word1, self.word2, self.word3, self.word4, self.word5, self.word6]
-    @IBOutlet weak var imageIndicator: UIImageView!
+
+    @IBOutlet weak var indicatorView: UIView!
+    var indicatorBoxes: Array<UIView> = []
+    var indicatorLayers: Array<CAGradientLayer> = []
+    var widths: Array<Int>  = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +36,149 @@ class CreateWalletViewController: UIViewController {
         }
         mnemonics = getAppDelegate().getMnemonicWords()!
         //createViews()
+        let maxWidth = self.view.frame.width - 50 - 60
+        let width = maxWidth / 6
+        widths = generateAllWidths(maxWidth: Int(maxWidth), blockWidth: Int(width))
+        createIndicators()
         loadWords()
+    }
+
+    func createIndicators() {
+        let height = 10
+        for index in 0..<24 {
+            let row = index / 6
+            let position = index % 6
+            let box = UIView()
+            box.frame = CGRect(x: 0, y: 0, width: widths[index], height: height)
+            box.backgroundColor = UIColor.clear
+            box.borderWidth = 1
+            box.borderColor = UIColor.customMatrixGreen()
+            box.translatesAutoresizingMaskIntoConstraints = false
+            indicatorBoxes.append(box)
+            indicatorView.addSubview(box)
+            if (row == 0) {
+                NSLayoutConstraint(item: box, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: indicatorView, attribute: NSLayoutAttribute.top, multiplier: 1, constant: 0).isActive = true
+            } else {
+                NSLayoutConstraint(item: box, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: indicatorBoxes[index - 6], attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 15).isActive = true
+            }
+            if (position == 0) {
+                NSLayoutConstraint(item: box, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: indicatorView, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: 0).isActive = true
+            } else {
+                NSLayoutConstraint(item: box, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: indicatorBoxes[index - 1], attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: 10).isActive = true
+            }
+            NSLayoutConstraint(item: box, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.width, multiplier: 1, constant: CGFloat(widths[index])).isActive = true
+            NSLayoutConstraint(item: box, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.height, multiplier: 1, constant: CGFloat(height)).isActive = true
+            //width height
+        }
+    }
+
+    func generateAllWidths(maxWidth: Int, blockWidth: Int) -> Array<Int> {
+        var tmp: Array<Int> = []
+        for _ in 0..<4 {
+            tmp.append(contentsOf: generateWidthsForRow(maxWidth: maxWidth, blockWidth: blockWidth))
+        }
+        return tmp
+    }
+
+    func generateWidthsForRow(maxWidth: Int, blockWidth: Int) -> Array<Int>{
+        var tmp: Array<Int> = []
+        var sum = maxWidth + 1
+        while (sum > maxWidth) {
+            sum = 0
+            tmp.removeAll()
+            for _ in 0..<6 {
+                let width = randomNumber(MIN: blockWidth - 15, MAX: blockWidth + 15)
+                tmp.append(width)
+                sum += width
+            }
+        }
+        return tmp
+    }
+
+    func randomNumber(MIN: Int, MAX: Int)-> Int{
+        var list : [Int] = []
+        for i in MIN...MAX {
+            list.append(i)
+        }
+        return list[Int(arc4random_uniform(UInt32(list.count)))]
+    }
+
+    func animateRow(row: Int) {
+        let start = row * 6
+        var column = 0
+            for index in start..<start+6 {
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .milliseconds(column * 500)) {
+                    self.animateColorChange(abc: self.indicatorBoxes[index])
+                }
+                column += 1
+            }
+    }
+
+    func animateBackwardsRow(row: Int) {
+        var column = 0
+        let count = indicatorLayers.count
+        let start = count - 6 > 0 ? count - 6 : 0
+        for index in start..<count {
+            indicatorLayers[index].removeFromSuperlayer()
+            column += 1
+        }
+        for _ in start..<count {
+            indicatorLayers.removeLast()
+        }
+    }
+
+    func animateColorChange(abc: UIView) {
+
+        let startLocations = [0, 0]
+        let endLocations = [1, 2]
+
+        let layer = CAGradientLayer()
+        layer.colors = [UIColor.customMatrixGreen().cgColor, UIColor.clear.cgColor]
+        layer.frame = abc.bounds
+        layer.locations = startLocations as [NSNumber]
+        layer.startPoint = CGPoint(x: 0.0, y: 1.0)
+        layer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        abc.layer.addSublayer(layer)
+        indicatorLayers.append(layer)
+
+        let anim = CABasicAnimation(keyPath: "locations")
+        anim.fromValue = startLocations
+        anim.toValue = endLocations
+        anim.duration = 0.5
+        layer.add(anim, forKey: "loc")
+        layer.locations = endLocations as [NSNumber]
+    }
+
+    func animateColorChangeBackwards(abc: UIView) {
+
+        let endLocations = [0, 0]
+        let startLocations = [1, 2]
+
+        let layer = CAGradientLayer()
+        layer.colors = [UIColor.customTitaniumDark().cgColor, UIColor.customTitaniumDark().cgColor]
+        layer.frame = abc.bounds
+        layer.locations = startLocations as [NSNumber]
+        layer.endPoint = CGPoint(x: 0.0, y: 1.0)
+        layer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        abc.layer.addSublayer(layer)
+        indicatorLayers.append(layer)
+
+        let anim = CABasicAnimation(keyPath: "locations")
+        anim.fromValue = endLocations
+        anim.toValue = startLocations
+        anim.duration = 0.5
+        layer.add(anim, forKey: "loc")
+        layer.locations = endLocations as [NSNumber]
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animateRow(row: 0)
     }
 
     override func viewDidLayoutSubviews() {
@@ -45,6 +191,7 @@ class CreateWalletViewController: UIViewController {
             self.performSegue(withIdentifier: "next", sender: nil)
         } else {
             pageCounter += 1
+            animateRow(row: pageCounter)
             loadWords()
         }
     }
@@ -59,26 +206,18 @@ class CreateWalletViewController: UIViewController {
             arrayLabels[index % 6].attributedText = formattedString
             arrayLabels[index % 6].sizeToFit()
         }
-        if(pageCounter == 0) {
-            imageIndicator.image = #imageLiteral(resourceName: "rowone")
-        } else if (pageCounter == 1) {
-             imageIndicator.image = #imageLiteral(resourceName: "rowtwo")
-        } else if (pageCounter == 2) {
-             imageIndicator.image = #imageLiteral(resourceName: "rowfour")
-        } else if (pageCounter == 3) {
-            imageIndicator.image = #imageLiteral(resourceName: "rowfour")
-        }
      }
 
     @IBAction func backButtonClicked(_ sender: Any) {
         if(pageCounter == 0) {
             navigationController?.popViewController(animated: true)
         } else {
+            animateBackwardsRow(row: pageCounter)
             pageCounter -= 1
             loadWords()
         }
     }
-    
+
     func createViews() {
         let screenSize: CGRect = UIScreen.main.bounds
         let viewWidth = (screenSize.width - 40) / 4
