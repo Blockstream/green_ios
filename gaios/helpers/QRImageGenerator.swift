345ddf61a9e40dcf8e9c683e067803c64c7d9c5e
Yostra <straja.mark@gmail.com>
2018-08-13 22:38:23 -0700

    simplify

--- gaios/helpers/QRImageGenerator.swift
+++ gaios/helpers/QRImageGenerator.swift
@@ -11,9 +11,8 @@ import UIKit
 
 class QRImageGenerator {
     
-    static func imageForAddress(address: String, frame: CGRect) -> UIImage? {
-        let formatted: String = "bitcoin:" + address
-        let data = formatted.data(using: String.Encoding.ascii, allowLossyConversion: false)
+    static func imageForText(text: String, frame: CGRect) -> UIImage? {
+        let data = text.data(using: String.Encoding.ascii, allowLossyConversion: false)
 
         let filter = CIFilter(name: "CIQRCodeGenerator")
         guard let colorFilter = CIFilter(name: "CIFalseColor") else { return nil }
@@ -31,30 +30,6 @@ class QRImageGenerator {
         let scaleY = frame.size.height / qrCodeImage.extent.size.height
         let scaledImage = qrCodeImage.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
         
-        return UIImage(ciImage: scaledImage)
-    }
-
-    static func imageForAddressAndAmount(address: String, amount: Double, frame: CGRect) -> UIImage? {
-        var formatted: String = "bitcoin:" + address
-        formatted += String(format: "?amount=%f", amount)
-        let data = formatted.data(using: String.Encoding.ascii, allowLossyConversion: false)
-        
-        let filter = CIFilter(name: "CIQRCodeGenerator")
-        guard let colorFilter = CIFilter(name: "CIFalseColor") else { return nil }
-        filter!.setValue(data, forKey: "inputMessage")
-        filter!.setValue("Q", forKey: "inputCorrectionLevel")
-        colorFilter.setValue(filter!.outputImage, forKey: "inputImage")
-        colorFilter.setValue(CIColor(color: UIColor.customMatrixGreen()), forKey: "inputColor1") // Background white
-        colorFilter.setValue(CIColor(color: UIColor.customTitaniumDark()), forKey: "inputColor0")
-
-        guard let qrCodeImage = colorFilter.outputImage
-            else {
-                return nil
-        }
-        let scaleX = frame.size.width / qrCodeImage.extent.size.width
-        let scaleY = frame.size.height / qrCodeImage.extent.size.height
-        let scaledImage = qrCodeImage.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
-
         return UIImage(ciImage: scaledImage)
     }
 }
