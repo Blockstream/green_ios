e8549865461e50d9dd12f3c4ea76259f1d290ba9
Yostra <straja.mark@gmail.com>
2018-08-13 22:36:58 -0700

    regenerate qr code when amount changes

--- gaios/ReceiveBtcViewController.swift
+++ gaios/ReceiveBtcViewController.swift
@@ -11,21 +11,27 @@ import UIKit
 import PromiseKit
 
 class ReceiveBtcViewController: UIViewController {
-    
+
     @IBOutlet weak var walletAddressLabel: UILabel!
     @IBOutlet weak var walletQRCode: UIImageView!
     var receiveAddress: String? = nil
     @IBOutlet weak var amountTextfield: UITextField!
+    @IBOutlet weak var estimateLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.isHidden = true
         walletAddressLabel.text = receiveAddress
-        walletQRCode.image = QRImageGenerator.imageForAddress(address: receiveAddress!, frame: walletQRCode.frame)
+        let uri = bip21Helper.btcURIforAddress(address: receiveAddress!)
+        walletQRCode.image = QRImageGenerator.imageForText(text: uri, frame: walletQRCode.frame)
         amountTextfield.attributedPlaceholder = NSAttributedString(string: "0.00",
                                                              attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+
+        amountTextfield.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+
+        self.hideKeyboardWhenTappedAround()
     }
-    
+
     func generateQRCode(_ text: String, _ frame: CGRect) -> UIImage {
         let data = text.data(using: String.Encoding.ascii, allowLossyConversion: false)
         
@@ -37,10 +43,25 @@ class ReceiveBtcViewController: UIViewController {
         let scaleX = frame.size.width / image.extent.size.width
         let scaleY = frame.size.height / image.extent.size.height
         let scaledImage = image.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
-        
+
         return UIImage(ciImage: scaledImage)
     }
-    
+
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        let btc_amount: String = textField.text!
+
+        guard let btc_amount_double = Double(btc_amount) else {
+            estimateLabel.text = "~0.00 USD"
+            return
+        }
+
+        walletQRCode.image = QRImageGenerator.imageForText(text: bip21Helper.btcURIforAmnount(address:self.receiveAddress!, amount: btc_amount_double), frame: walletQRCode.frame)
+
+        let satoshi: Int = Int(btc_amount_double * 100000000)
+        let usd_amount = AccountStore.shared.satoshiToUSD(amount: satoshi)
+        estimateLabel.text = String(format: "~%.2f USD", usd_amount)
+    }
+
     @IBAction func copyButtonClicked(_ sender: Any) {
         UIPasteboard.general.string = receiveAddress
     }
