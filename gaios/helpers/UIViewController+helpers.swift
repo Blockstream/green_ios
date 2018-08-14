c8d8dd137740eba31ea3b400197f422b08d5cf88
Yostra <straja.mark@gmail.com>
2018-08-13 22:35:02 -0700

    helpers

---
+++ gaios/helpers/UIViewController+helpers.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+helpers.swift
+//  gaios
+//
+//  Created by Strahinja Markovic on 8/13/18.
+//  Copyright © 2018 Goncalo Carvalho. All rights reserved.
+//
+
+import Foundation
---
+++ gaios/helpers/bip21Helper.swift
@@ -0,0 +1,9 @@
+//
+//  bip21Helper.swift
+//  gaios
+//
+//  Created by Strahinja Markovic on 8/13/18.
+//  Copyright © 2018 Goncalo Carvalho. All rights reserved.
+//
+
+import Foundation
