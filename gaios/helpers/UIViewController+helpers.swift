0b46fb5b6acbb84c3e95f05324f419b5f41320b2
Yostra <straja.mark@gmail.com>
2018-08-13 22:35:47 -0700

    hmm helpers

--- gaios/helpers/UIViewController+helpers.swift
+++ gaios/helpers/UIViewController+helpers.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
--- gaios/helpers/bip21Helper.swift
+++ gaios/helpers/bip21Helper.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+class bip21Helper {
+
+    static func btcURIforAmnount(address: String, amount: Double) ->String {
+        let result = String(format: "bitcoin:%@?amount=%.2f", address, amount)
+        return result
+    }
+
+    static func btcURIforAddress(address: String) ->String {
+        let result = String(format: "bitcoin:%@", address)
+        return result
+    }
+}
