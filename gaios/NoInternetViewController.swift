48ba8cbf71f213fec8ac1252bbdec353f850bda9
Yostra <straja.mark@gmail.com>
2018-09-09 18:16:25 -0700

    no internet matrix

---
+++ gaios/NoInternetViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NoInternetViewController.swift
+//  gaios
+//
+//  Created by Strahinja Markovic on 9/9/18.
+//  Copyright © 2018 Goncalo Carvalho. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+class NoInternetViewController: UIViewController {
+
+    @IBOutlet weak var bottomButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func refreshClicked(_ sender: Any) {
+    }
+
+}
