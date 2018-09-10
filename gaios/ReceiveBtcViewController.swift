c34e479f658241fa4c0626b662f7ba53843a9bf0
Yostra <straja.mark@gmail.com>
2018-09-10 14:21:58 -0700

    receive selection type fiat <->btc

--- gaios/ReceiveBtcViewController.swift
+++ gaios/ReceiveBtcViewController.swift
@@ -19,6 +19,9 @@ class ReceiveBtcViewController: UIViewController {
     @IBOutlet weak var estimateLabel: UILabel!
     @IBOutlet weak var shareButton: UIButton!
     @IBOutlet weak var fiatSwitchButton: UIButton!
+    var selectedType = TransactionType.FIAT
+    var amount: Double = 0
+    @IBOutlet weak var typeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +35,52 @@ class ReceiveBtcViewController: UIViewController {
         amountTextfield.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
 
         self.hideKeyboardWhenTappedAround()
+        setButton()
+        updateType()
+        updateEstimate()
     }
 
     @IBAction func switchButtonClicked(_ sender: Any) {
+        changeType()
+    }
+
+    func changeType() {
+        if (selectedType == TransactionType.BTC) {
+            selectedType = TransactionType.FIAT
+        } else {
+            selectedType = TransactionType.BTC
+        }
+        setButton()
+        updateType()
+        updateEstimate()
+    }
+
+    func setButton() {
+        if (selectedType == TransactionType.BTC) {
+            fiatSwitchButton.setTitle(SettingsStore.shared.getDenominationSettings(), for: UIControlState.normal)
+            fiatSwitchButton.backgroundColor = UIColor.customMatrixGreen()
+            fiatSwitchButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        } else {
+            fiatSwitchButton.setTitle(SettingsStore.shared.getCurrencyString(), for: UIControlState.normal)
+            fiatSwitchButton.backgroundColor = UIColor.clear
+            fiatSwitchButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        }
+    }
+
+    func updateType() {
+        if (selectedType == TransactionType.BTC) {
+            typeLabel.text = SettingsStore.shared.getDenominationSettings()
+        } else {
+            typeLabel.text = SettingsStore.shared.getCurrencyString()
+        }
+    }
+
+    func updateEstimate() {
+        if (selectedType == TransactionType.BTC) {
+            estimateLabel.text = "~"
+        } else {
+            estimateLabel.text = "~"
+        }
     }
 
     override func viewDidLayoutSubviews() {
@@ -57,6 +103,12 @@ class ReceiveBtcViewController: UIViewController {
         estimateLabel.text = String(format: "~%.2f USD", usd_amount)
     }
 
+    @IBAction func shareButtonClicked(_ sender: Any) {
+        let activityViewController = UIActivityViewController(activityItems: [receiveAddress!] , applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+
     @IBAction func copyButtonClicked(_ sender: Any) {
         UIPasteboard.general.string = receiveAddress
     }
@@ -64,4 +116,9 @@ class ReceiveBtcViewController: UIViewController {
     @IBAction func backButtonClicked(_ sender: Any) {
         _ = navigationController?.popViewController(animated: true)
     }
+}
+
+public enum TransactionType: UInt32 {
+    case BTC = 0
+    case FIAT = 1
 }
