f9471c00ca6029caad7fcd9c75f05c807364284b
Yostra <straja.mark@gmail.com>
2018-10-04 14:52:43 -0700

    url extenstion

--- gaios/helpers/String+random.swift
+++ gaios/helpers/String+random.swift
@@ -99,3 +99,15 @@ extension NSMutableAttributedString {
     }
 
 }
+
+extension URL {
+    public var queryItems: [String: String] {
+        var params = [String: String]()
+        return URLComponents(url: self, resolvingAgainstBaseURL: false)?
+            .queryItems?
+            .reduce([:], { (_, item) -> [String: String] in
+                params[item.name] = item.value
+                return params
+            }) ?? [:]
+    }
+}
