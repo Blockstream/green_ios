a1cc7908cf75aa01628b3fd2d56965ebbc8117f6
Yostra <straja.mark@gmail.com>
2018-10-09 20:02:15 -0700

    enable pin/face/touch after restore

---
+++ gaios/WelcomeBackViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeBackViewController.swift
+//  gaios
+//
+//  Created by Strahinja Markovic on 10/9/18.
+//  Copyright © 2018 Goncalo Carvalho. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WelcomeBackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
