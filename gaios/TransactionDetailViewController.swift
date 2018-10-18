112c5cc6c48481d9c5990f5d55e7e2e12111025e
Yostra <straja.mark@gmail.com>
2018-10-18 13:14:56 -0700

    show increase fee button if no confirmations

--- gaios/TransactionDetailViewController.swift
+++ gaios/TransactionDetailViewController.swift
@@ -18,6 +18,9 @@ class TransactionDetailViewController: UIViewController {
     @IBOutlet weak var feeLabel: UILabel!
     @IBOutlet weak var memoLabel: UILabel!
     @IBOutlet weak var warniniglabel: UILabel!
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    @IBOutlet weak var feeButton: UIButton!
+    @IBOutlet weak var bottomLabel: UILabel!
 
     var transaction: TransactionItem? = nil
 
@@ -28,7 +31,11 @@ class TransactionDetailViewController: UIViewController {
         feeLabel.text = feeText(fee: (transaction?.fee)!, size: (transaction?.size)!)
         memoLabel.text = transaction?.memo
         dateLabel.text = transaction?.date
+        feeButton.isHidden = true
+        bottomLabel.isHidden = true
         if(transaction?.blockheight == 0) {
+            feeButton.isHidden = false
+            bottomLabel.isHidden = false
             warniniglabel.text = "Unconfirmed transaction, please wait for block confirmations to gain trust in this transaction "
         } else if (AccountStore.shared.getBlockheight() - (transaction?.blockheight)! < 6) {
             let blocks = AccountStore.shared.getBlockheight() - (transaction?.blockheight)! + 1
@@ -36,7 +43,9 @@ class TransactionDetailViewController: UIViewController {
         } else {
             warniniglabel.isHidden = true
         }
-        //something
+    }
+
+    @IBAction func increaseFeeClicked(_ sender: Any) {
     }
 
     func feeText(fee: UInt32, size: UInt32) -> String {
@@ -46,6 +55,8 @@ class TransactionDetailViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        feeButton.layoutIfNeeded()
+        feeButton.applyGradient(colours: [UIColor.customMatrixGreen(), UIColor.customMatrixGreenDark()])
     }
 
     @IBAction func backButtonClicked(_ sender: Any) {
