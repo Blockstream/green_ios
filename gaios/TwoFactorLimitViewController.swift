bcc94fc86841b29a1382e5cf67ae21e49f12cd23
Yostra <straja.mark@gmail.com>
2018-10-18 20:39:34 -0700

    limits

--- gaios/TwoFactorLimitViewController.swift
+++ gaios/TwoFactorLimitViewController.swift
@@ -11,16 +11,98 @@ import UIKit
 
 class TwoFactorLimitViewController: UIViewController {
 
+    @IBOutlet weak var limitTextField: UITextField!
+    @IBOutlet weak var setLimitButton: UIButton!
+    @IBOutlet weak var fiatButton: UIButton!
+    @IBOutlet weak var limitButtonConstraint: NSLayoutConstraint!
+    var fiat: Bool = true
+    @IBOutlet weak var descriptionLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(TwoFactorLimitViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(TwoFactorLimitViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        setButton()
+        if(!AccountStore.shared.isTwoFactorEnabled()) {
+            setLimitButton.isEnabled = false
+            descriptionLabel.text = "You need to enable Two-Factor Authentication before setting the limit"
+        }
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let position =  self.view.frame.height - keyboardSize.height - setLimitButton.frame.height
+            var keyboardHeight = keyboardSize.height
+            if #available(iOS 11.0, *) {
+                let bottomInset = view.safeAreaInsets.bottom
+                keyboardHeight -= bottomInset
+            }
+            limitButtonConstraint.constant = keyboardHeight
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            limitButtonConstraint.constant = 0
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        limitTextField.becomeFirstResponder()
+    }
+
     @IBAction func backButtonClicked(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
 
+    @IBAction func setLimitClicked(_ sender: Any) {
+        if let amount = Double(limitTextField.text!) {
+            do {
+                /*var details = [String:String]()
+                if(fiat) {
+                    details["is_fiat"] = "true"
+                    details["fiat"] = String(amount)
+                } else {
+                    details["is_fiat"] = "false"
+                    details["satoshi"] = String(amount)
+                }
+                let factor = try getSession().setTwoFactorLimit(details: details)
+                let status = try factor.getStatus()
+                let statusString = status!["status"] as! String
+                if (statusString == "call") {
+                    let call = try factor.call()
+                    let callStatus = try factor.getStatus()
+                }*/
+            } catch {
+                print("couldnt set limit")
+            }
+        }
+    }
+
+    func setButton() {
+        if(fiat) {
+            fiatButton.setTitle(SettingsStore.shared.getCurrencyString(), for: UIControlState.normal)
+            fiatButton.backgroundColor = UIColor.clear
+            fiatButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        } else {
+            fiatButton.setTitle(SettingsStore.shared.getDenominationSettings(), for: UIControlState.normal)
+            fiatButton.backgroundColor = UIColor.customMatrixGreen()
+            fiatButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        }
+    }
+
+    @IBAction func fiatButtonClicked(_ sender: Any) {
+        if(fiat) {
+            fiat = !fiat
+            setButton()
+        } else {
+            fiat = !fiat
+            setButton()
+        }
+    }
 }
