8fa653c6cb14c3712699dc859cea51843689b203
Yostra <straja.mark@gmail.com>
2018-10-24 00:23:50 -0700

    trie

---
+++ gaios/helpers/Trie.swift
@@ -0,0 +1,121 @@
+
+import Foundation
+
+class TrieNode<T: Hashable> {
+    var value: T?
+    weak var parentNode: TrieNode?
+    var children: [T: TrieNode] = [:]
+    var isTerminating = false
+    var isLeaf: Bool {
+        return children.count == 0
+    }
+
+    init(value: T? = nil, parentNode: TrieNode? = nil) {
+        self.value = value
+        self.parentNode = parentNode
+    }
+    
+    func add(value: T) {
+        guard children[value] == nil else {
+            return
+        }
+        children[value] = TrieNode(value: value, parentNode: self)
+    }
+}
+
+
+class Trie: NSObject, NSCoding {
+    typealias Node = TrieNode<Character>
+    public var count: Int {
+        return wordCount
+    }
+    public var isEmpty: Bool {
+        return wordCount == 0
+    }
+    public var words: [String] {
+        return wordsInSubtrie(rootNode: root, partialWord: "")
+    }
+    fileprivate let root: Node
+    fileprivate var wordCount: Int
+    
+    override init() {
+        root = Node()
+        wordCount = 0
+        super.init()
+    }
+    
+    required convenience init?(coder decoder: NSCoder) {
+        self.init()
+        let words = decoder.decodeObject(forKey: "words") as? [String]
+        for word in words! {
+            self.insert(word: word)
+        }
+    }
+
+    func encode(with coder: NSCoder) {
+        coder.encode(self.words, forKey: "words")
+    }
+
+    func insert(word: String) {
+        guard !word.isEmpty else {
+            return
+        }
+        var currentNode = root
+        for character in word.lowercased() {
+            if let childNode = currentNode.children[character] {
+                currentNode = childNode
+            } else {
+                currentNode.add(value: character)
+                currentNode = currentNode.children[character]!
+            }
+        }
+        guard !currentNode.isTerminating else {
+            return
+        }
+        wordCount += 1
+        currentNode.isTerminating = true
+    }
+    
+    private func findLastNodeOf(word: String) -> Node? {
+        var currentNode = root
+        for character in word.lowercased() {
+            guard let childNode = currentNode.children[character] else {
+                return nil
+            }
+            currentNode = childNode
+        }
+        return currentNode
+    }
+    
+    func findWordsWithPrefix(prefix: String) -> [String] {
+        var words = [String]()
+        let prefixLowerCased = prefix.lowercased()
+        if let lastNode = findLastNodeOf(word: prefixLowerCased) {
+            if lastNode.isTerminating {
+                words.append(prefixLowerCased)
+            }
+            for childNode in lastNode.children.values {
+                let childWords = wordsInSubtrie(rootNode: childNode, partialWord: prefixLowerCased)
+                words += childWords
+            }
+        }
+        return words
+    }
+    
+    fileprivate func wordsInSubtrie(rootNode: Node, partialWord: String) -> [String] {
+        var subtrieWords = [String]()
+        var previousLetters = partialWord
+        if let value = rootNode.value {
+            previousLetters.append(value)
+        }
+        if rootNode.isTerminating {
+            subtrieWords.append(previousLetters)
+        }
+        for childNode in rootNode.children.values {
+            let childWords = wordsInSubtrie(rootNode: childNode, partialWord: previousLetters)
+            subtrieWords += childWords
+        }
+        return subtrieWords
+    }
+}
+
