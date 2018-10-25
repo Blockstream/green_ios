86e26f5e2ab94cab9faa711c26177cda1229c37e
Yostra <straja.mark@gmail.com>
2018-10-24 21:19:13 -0700

    perform segues

--- gaios/WatchOnlySignIn.swift
+++ gaios/WatchOnlySignIn.swift
@@ -1,8 +1,9 @@
 
 import Foundation
 import UIKit
+import NVActivityIndicatorView
 
-class WatchOnlySignIn: UIViewController {
+class WatchOnlySignIn: UIViewController, NVActivityIndicatorViewable {
 
     @IBOutlet weak var titlelabel: UILabel!
     @IBOutlet weak var usernameTextField: UITextField!
@@ -26,6 +27,49 @@ class WatchOnlySignIn: UIViewController {
         passwordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: passwordTextField.frame.height))
         usernameTextField.leftViewMode = .always
         passwordTextField.leftViewMode = .always
+    }
+
+    @IBAction func loginButtonClicked(_ sender: Any) {
+        let size = CGSize(width: 30, height: 30)
+        let message = NSLocalizedString("plogging_in_please_wait", comment: "")
+        startAnimating(size, message: message, messageFont: nil, type: NVActivityIndicatorType.ballRotateChase)
+
+        let username = usernameTextField.text
+        let password = passwordTextField.text
+
+        wrap{
+            try getSession().loginWatchOnly(username: username!, password: password!)
+        }.done{
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.7) {
+                self.stopAnimating()
+                AccountStore.shared.initializeAccountStore()
+                self.performSegue(withIdentifier: "main", sender: nil)
+            }
+        }.catch { error in
+            DispatchQueue.main.async{
+                let message = NSLocalizedString("plogin_failed", comment: "")
+                NVActivityIndicatorPresenter.sharedInstance.setMessage(message)
+            }
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.7) {
+                self.stopAnimating()
+            }
+        }
+    }
+
+    @IBAction func forgotMyPasswordClicked(_ sender: Any) {
+        DispatchQueue.main.async{
+            let message = NSLocalizedString("plog_in_using_seed", comment: "")
+            let size = CGSize(width: 30, height: 30)
+            self.startAnimating(size, message: message, messageFont: nil, type: NVActivityIndicatorType.blank)
+        }
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+            self.stopAnimating()
+            self.performSegue(withIdentifier: "enterMnemonics", sender: "")
+        }
+    }
+
+    @IBAction func cacnelButtonClicked(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
 
 }
