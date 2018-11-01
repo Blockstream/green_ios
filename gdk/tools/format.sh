83ae54e782f0cfa56d1619fe18df1287476bca8e
Luca Vaccaro <luck87@gmail.com>
2018-11-01 18:22:56 +0100

    Remove internal copy of gdk

--- gdk/tools/format.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if [ -d build-clang ]; then
-    BUILD_DIR=build-clang
-else
-    BUILD_DIR=build-gcc
-fi
-(cd $BUILD_DIR && ninja src/clang-format && ninja tests/clang-format)
