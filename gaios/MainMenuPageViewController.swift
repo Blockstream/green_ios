5e80fe3384ca9fd1c2a3729efed92fff362c24e2
Luca Vaccaro <luck87@gmail.com>
2018-11-12 15:42:05 +0100

    Fix unintuitive Swipe between Pages

--- gaios/MainMenuPageViewController.swift
+++ gaios/MainMenuPageViewController.swift
@@ -131,7 +131,7 @@ class MainMenuPageViewController: UIPageViewController, UIPageViewControllerDele
 
         let previousIndex = viewControllerIndex - 1
 
-        guard previousIndex >= 0          else { return pages.last }
+        guard previousIndex >= 0          else { return nil }
 
         guard pages.count > previousIndex else { return nil        }
 
@@ -144,7 +144,7 @@ class MainMenuPageViewController: UIPageViewController, UIPageViewControllerDele
 
         let nextIndex = viewControllerIndex + 1
 
-        guard nextIndex < pages.count else { return pages.first }
+        guard nextIndex < pages.count else { return nil }
 
         guard pages.count > nextIndex else { return nil         }
         return pages[nextIndex]
