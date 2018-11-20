8c34902efd84bc9ccab1215a3164621ecdc08fff
Luca Vaccaro <luck87@gmail.com>
2018-11-20 17:56:00 +0100

    Remove subtitle in the first view

--- gaios/Onboarding/InitialViewController.swift
+++ gaios/Onboarding/InitialViewController.swift
@@ -9,13 +9,11 @@ class InitialViewController: UIViewController, NetworkDelegate {
     @IBOutlet weak var topButton: DesignableButton!
     @IBOutlet weak var networkButton: UIButton!
     @IBOutlet weak var restoreWalletbutton: UIButton!
-    @IBOutlet weak var titleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         topButton.setTitle(NSLocalizedString("id_create_wallet", comment: ""), for: .normal)
         restoreWalletbutton.setTitle(NSLocalizedString("id_restore_an_existing_wallet", comment: ""), for: .normal)
-        titleLabel.text = NSLocalizedString("id_bitcoins_most_secure_wallet", comment: "")
     }
 
     override func viewWillAppear(_ animated: Bool) {
