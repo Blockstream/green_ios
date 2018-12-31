15c1b3bb74382e888506e4cea5ad64f7392e5032
Goncalo Carvalho <goncalo@blockstream.io>
2018-12-31 18:02:39 +0000

    initial mnemonic collection cell

--- gaios/Onboarding/EnterMnemonicsViewController.swift
+++ gaios/Onboarding/EnterMnemonicsViewController.swift
@@ -1,38 +1,48 @@
-import Foundation
 import UIKit
 import NVActivityIndicatorView
 import PromiseKit
 
-class EnterMnemonicsViewController: QRCodeReaderViewController, UITextFieldDelegate, SuggestionsDelegate, NVActivityIndicatorViewable {
+class EnterMnemonicsViewController: QRCodeReaderViewController, SuggestionsDelegate, NVActivityIndicatorViewable {
 
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var scanBarButton: UIBarButtonItem!
+    @IBOutlet weak var mnemonicWords: UICollectionView!
 
     let WL = getBIP39WordList()
 
-    var textFields: Array<UITextField> = []
-    var box:UIView = UIView()
-    var constraint: NSLayoutConstraint? = nil
     var suggestions: KeyboardSuggestions? = nil
-    var mnemonic: [String]? = nil
+    var mnemonic = [String](repeating: String(), count: 24)
     var QRCodeReader = UIView()
     var QRBackgroundView = UIView()
-    var adaptToSmallScreen = false
     var isScannerVisible = false
+
+    var currIndexPath: IndexPath? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        adaptToSmallScreen = self.view.frame.height == 568
-        createUI()
+
         title = NSLocalizedString("id_enter_your_wallet_mnemonic", comment: "")
         doneButton.setTitle(NSLocalizedString("id_done", comment: ""), for: .normal)
+
+        mnemonicWords.delegate = self
+        mnemonicWords.dataSource = self
+
         createSuggestionView()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        doneButton.backgroundColor = UIColor.customTitaniumLight()
-        doneButton.isUserInteractionEnabled = false
+        updateDoneButton(false)
+    }
+
+    func updateDoneButton(_ enable: Bool) {
+        if !enable {
+            doneButton.applyHorizontalGradient(colours: [UIColor.customTitaniumMedium(), UIColor.customTitaniumLight()])
+            doneButton.isUserInteractionEnabled = false
+        } else {
+            doneButton.applyHorizontalGradient(colours: [UIColor.customMatrixGreenDark(), UIColor.customMatrixGreen()])
+            doneButton.isUserInteractionEnabled = true
+        }
     }
 
     @objc override func keyboardWillShow(notification: NSNotification) {
@@ -40,7 +50,10 @@ class EnterMnemonicsViewController: QRCodeReaderViewController, UITextFieldDeleg
 
         let userInfo = notification.userInfo
         let keyboardFrame = userInfo?[UIKeyboardFrameEndUserInfoKey] as! CGRect
-        self.suggestions!.frame = CGRect(x: 0, y: view.frame.height - keyboardFrame.height - 40, width: view.frame.width, height: 40)
+        let contentInset = UIEdgeInsetsMake(0.0, 0.0, keyboardFrame.height, 0.0)
+        mnemonicWords.contentInset = contentInset
+        mnemonicWords.scrollIndicatorInsets = contentInset
+        suggestions!.frame = CGRect(x: 0, y: view.frame.height - keyboardFrame.height - 40, width: view.frame.width, height: 40)
     }
 
     @objc override func keyboardWillHide(notification: NSNotification) {
@@ -56,23 +69,13 @@ class EnterMnemonicsViewController: QRCodeReaderViewController, UITextFieldDeleg
     }
 
     func suggestionWasTapped(suggestion: String) {
-        for index in 0..<textFields.count {
-            let textField = textFields[index]
-            if textField.isFirstResponder {
-                textField.text = suggestion
-                if(index < textFields.count - 1) {
-                    let next = textFields[index+1]
-                    next.becomeFirstResponder()
-                }
-                suggestions!.isHidden = true
-                break
-            }
-        }
+        mnemonic[currIndexPath!.row + currIndexPath!.section * 3] = suggestion
+        mnemonicWords.reloadData()
     }
 
     func getMnemonicString() -> Promise<String> {
         return Promise { seal in
-            seal.fulfill(textFields.compactMap { $0.text! }.joined(separator: " ").lowercased())
+            seal.fulfill(mnemonic.joined(separator: " ").lowercased())
         }
     }
 
@@ -84,26 +87,6 @@ class EnterMnemonicsViewController: QRCodeReaderViewController, UITextFieldDeleg
         let words = getSuggestions(prefix: prefix)
         self.suggestions!.setSuggestions(suggestions: words)
         self.suggestions!.isHidden = words.isEmpty
-    }
-
-    func doneButtonEnable() {
-        doneButton.applyHorizontalGradient(colours: [UIColor.customMatrixGreen(), UIColor.customMatrixGreenDark()])
-        doneButton.isUserInteractionEnabled = true
-    }
-
-    @objc func textFieldDidChange(_ textField: UITextField) {
-        checkTextfield(textField: textField)
-        if !(textField.text?.isEmpty ?? true) {
-            updateSuggestions(prefix: textField.text!)
-        } else {
-            suggestions!.isHidden = true
-        }
-        for field in textFields {
-            if field.text?.isEmpty ?? true {
-                return
-            }
-        }
-        doneButtonEnable()
     }
 
     fileprivate func login() {
@@ -150,96 +133,6 @@ class EnterMnemonicsViewController: QRCodeReaderViewController, UITextFieldDeleg
     func checkTextfield(textField: UITextField) {
         let suggestions = getSuggestions(prefix: textField.text!)
         textField.textColor = suggestions.isEmpty ? UIColor.red : UIColor.white
-    }
-
-    func checkMnemonics() {
-        textFields.forEach { checkTextfield(textField: $0) }
-    }
-
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.endEditing(true)
-        return false
-    }
-
-    func createBlock(number: Int) -> UIView {
-        let numberString = String(format: "%d", number+1) //loop start with 0, ui starts with 1
-        let block:UIView = UIView()
-        block.frame = CGRect(x: 0, y: 0, width: 8, height: 14)
-        block.translatesAutoresizingMaskIntoConstraints = false
-
-        let label: UILabel = UILabel()
-        label.frame = CGRect(x: 0, y: 0, width: 8, height: 14)
-        label.text = numberString
-        label.textColor = UIColor.customMatrixGreen()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        block.addSubview(label)
-        NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: block, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: block, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.height, multiplier: 1, constant: 14).isActive = true
-        let size = label.sizeThatFits(CGSize(width: 25, height: 15))
-        NSLayoutConstraint(item: label, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.lessThanOrEqual, toItem: nil, attribute: NSLayoutAttribute.width, multiplier: 1, constant: size.width).isActive = true
-
-        let textField: TextField = TextField()
-        textField.frame = CGRect(x: 0, y: 0, width: 8, height: 14)
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.borderStyle = .none
-        textField.textColor = UIColor.white
-        textField.autocorrectionType = .no
-        textField.autocapitalizationType = .none
-        textField.spellCheckingType = .no
-        textField.adjustsFontSizeToFitWidth = true
-        textField.delegate = self
-        textField.returnKeyType = UIReturnKeyType.done
-        textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
-        block.addSubview(textField)
-        textFields.append(textField)
-
-        NSLayoutConstraint(item: textField, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: label, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: 5).isActive = true
-        NSLayoutConstraint(item: textField, attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal, toItem: block, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: -5).isActive = true
-        NSLayoutConstraint(item: textField, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: block, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: -1).isActive = true
-        NSLayoutConstraint(item: textField, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.height, multiplier: 1, constant: 30).isActive = true
-
-        let bottomLine:UIView = UIView()
-        bottomLine.frame = CGRect(x: 0, y: 0, width: 8, height: 14)
-        bottomLine.translatesAutoresizingMaskIntoConstraints = false
-        bottomLine.backgroundColor = UIColor.customTitaniumMedium()
-        block.addSubview(bottomLine)
-        NSLayoutConstraint(item: bottomLine, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: block, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 0).isActive = true
-        NSLayoutConstraint(item: bottomLine, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.height, multiplier: 1, constant: 1).isActive = true
-        NSLayoutConstraint(item: bottomLine, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: label, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: 5).isActive = true
-        NSLayoutConstraint(item: bottomLine, attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal, toItem: block, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: -5).isActive = true
-
-        return block
-    }
-
-    func createUI() {
-        let blockWidth = (view.frame.width - 32) / 4
-        box.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-        box.translatesAutoresizingMaskIntoConstraints = false
-        let rowMargin = adaptToSmallScreen ? 45 : 60
-        let height = rowMargin * 6
-        view.addSubview(box)
-        NSLayoutConstraint(item: box, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.width, multiplier: 0, constant: view.frame.width).isActive = true
-        NSLayoutConstraint(item: box, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.height, multiplier: 1, constant: CGFloat(height)).isActive = true
-        constraint = NSLayoutConstraint(item: box, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.top, multiplier: 1, constant: 18)
-        constraint!.isActive = true
-        NSLayoutConstraint(item: box, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: 0).isActive = true
-
-        for index in 0..<24 {
-            let row:Int = index / 4
-            let block = createBlock(number: index)
-            box.addSubview(block)
-
-            let leadingConstant:CGFloat = CGFloat(16 + CGFloat(index % 4) * blockWidth)
-
-            let topConstant:CGFloat = CGFloat(row * rowMargin)
-
-            NSLayoutConstraint(item: block, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.width, multiplier: 0, constant: blockWidth).isActive = true
-            NSLayoutConstraint(item: block, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.height, multiplier: 1, constant: 45).isActive = true
-            NSLayoutConstraint(item: block, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: box, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: leadingConstant).isActive = true
-            NSLayoutConstraint(item: block, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: box, attribute: NSLayoutAttribute.top, multiplier: 1, constant: topConstant).isActive = true
-            //add constraints tp block
-        }
     }
 
     @IBAction func startQRScan(_ sender: UIBarButtonItem) {
@@ -291,15 +184,67 @@ class EnterMnemonicsViewController: QRCodeReaderViewController, UITextFieldDeleg
 
     override func onQRCodeReadSuccess(result: String) {
         let words = result.split(separator: " ")
-        guard words.count == textFields.count else {
+        guard words.count == 24 || words.count == 27 else {
             return
         }
 
-        for (word, field) in zip(words, textFields) {
-            field.text = String(word)
-        }
+        words.enumerated().forEach { mnemonic[$0.0] = String($0.1) }
+        mnemonicWords.reloadData()
 
-        doneButtonEnable()
+        updateDoneButton(true)
         stopScan()
+    }
+}
+
+extension EnterMnemonicsViewController : UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "wordCell",
+                                                      for: indexPath) as! MnemonicWordCell
+        cell.wordLabel.text = String(indexPath.row + indexPath.section * 3 + 1)
+        cell.wordText.text = mnemonic[indexPath.row + indexPath.section * 3]
+        cell.delegate = self
+        return cell
+    }
+}
+
+extension EnterMnemonicsViewController : UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 8
+    }
+}
+
+extension EnterMnemonicsViewController : UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width / 3, height: 40)
+    }
+}
+
+extension EnterMnemonicsViewController : MnemonicWordCellDelegate {
+
+    func collectionView(valueChangedIn textField: UITextField, from cell: MnemonicWordCell) {
+        let text = textField.text?.isEmpty ?? true ? String() : textField.text!
+
+        currIndexPath = mnemonicWords.indexPath(for: cell)
+        mnemonic[currIndexPath!.row + currIndexPath!.section * 3] = text
+
+        checkTextfield(textField: textField)
+        if !text.isEmpty {
+            updateSuggestions(prefix: textField.text!)
+        } else {
+            suggestions!.isHidden = true
+        }
+        for word in mnemonic {
+            if word.isEmpty {
+                return
+            }
+        }
+        updateDoneButton(true)
     }
 }
