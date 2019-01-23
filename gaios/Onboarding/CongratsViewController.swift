f7b91c77f97bcf217c05497c2312da0ff116fef9
Luca Vaccaro <luck87@gmail.com>
2019-01-23 18:15:46 +0100

    Fix congratulations font size

--- gaios/Onboarding/CongratsViewController.swift
+++ gaios/Onboarding/CongratsViewController.swift
@@ -4,12 +4,14 @@ import UIKit
 class CongratsViewController : UIViewController {
 
     @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var subtext: UILabel!
     @IBOutlet weak var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
-        self.text.text = NSLocalizedString("id_congratulations", comment: "") + NSLocalizedString("id_your_green_wallet_is_now_ready", comment: "")
+        self.text.text = NSLocalizedString("id_congratulations", comment: "")
+        self.subtext.text = NSLocalizedString("id_your_green_wallet_is_now_ready", comment: "")
         self.button.setTitle(NSLocalizedString("id_next", comment: ""), for: .normal)
     }
 }
