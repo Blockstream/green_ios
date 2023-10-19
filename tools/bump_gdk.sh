a4d84b344a524bc20d24bc86583b0e217af88ce7
Luca Vaccaro <me@lvaccaro.com>
2023-10-20 00:16:01 +0200

    tools: update script to auto-bump gdk
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- tools/bump_gdk.sh
+++ tools/bump_gdk.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -e
 # Upgrades gdk to latest available at github.com/Blockstream/gdk
 
 function check_command() {
@@ -60,22 +60,37 @@ if [[ $TAGNAME = false ]]; then
   TAGNAME=$(curl https://api.github.com/repos/blockstream/gdk/releases/latest | jq -r .tag_name)
 fi
 
-NAME=$(curl https://api.github.com/repos/blockstream/gdk/releases/tags/$TAGNAME | jq -r .name)
+# Import Green key
+PGPKEY="04BEBF2E35A2AF2FFDF1FA5DE7F054AA2E76E792"
+gpg --keyserver pgp.mit.edu --recv-keys $PGPKEY
+# Download the key file
+curl -sL -o IOS_SHA256SUMS.asc https://github.com/Blockstream/gdk/releases/download/$TAGNAME/IOS_SHA256SUMS.asc
+# Check the signed key
+gpg --verify IOS_SHA256SUMS.asc
+# Inspect the key file
+gpg --keyid-format long --list-options show-keyring IOS_SHA256SUMS.asc
+
+
+update() {
+    TARFILE=$1
+    PLATFORM=$2
+    TARURL="https://github.com/Blockstream/gdk/releases/download/$TAGNAME/$TARFILE"
+    curl -sL -o $TARFILE $TARURL
+    SHA256_TAR=$(shasum -a256 $TARFILE | awk '{print $1;}')
+    SHA256_SUM=$(cat IOS_SHA256SUMS | grep $TARFILE | awk '{print $1;}')
+    if [ "$SHA256_TAR" != "$SHA256_SUM" ]; then
+        echo "Shasum ${TARFILE} mismatch"
+        exit 1
+    fi
+    sed -i '' -e "s/${PLATFORM}=.*/${PLATFORM}=\"${SHA256_TAR}\"/" ./tools/fetch_gdk_binaries.sh
+    rm $TARFILE
+}
+    
 sed -i '' -e "s/TAGNAME=.*/TAGNAME=\"${TAGNAME}\"/" ./tools/fetch_gdk_binaries.sh
-sed -i '' -e "s/TAGNAME=.*/TAGNAME=\"${TAGNAME}\"/" ./tools/build.sh
+update "gdk-iphone.tar.gz" "ARM_SHA256"
+update "gdk-iphone-sim.tar.gz" "ARM_SIM_SHA256"
+update "gdk-iphone-sim-x86_64.tar.gz" "X86_SIM_SHA256"
 
-for PLAT in "gdk-iphone" "gdk-iphone-sim"; do
-   TARURL=$(curl https://api.github.com/repos/blockstream/gdk/releases/tags/$TAGNAME | jq -r ".assets[].browser_download_url" | grep $PLAT.tar.gz)
-   TEMP=$(mktemp)
-   curl -sL -o $TEMP $TARURL
-   SHA256=$(shasum -a256 $TEMP | awk '{print $1;}')
-   echo $SHA256
-   if [[ "$PLAT" == "gdk-iphone" ]]; then
-      sed -i '' -e "s/SHA256=\".*/SHA256=\"${SHA256}\"/" ./tools/fetch_gdk_binaries.sh
-   else
-      sed -i '' -e "s/SHA256_IPHONESIM=.*/SHA256_IPHONESIM=\"${SHA256}\"/" ./tools/fetch_gdk_binaries.sh
-   fi
-done
-
-git add ./tools/build.sh ./tools/fetch_gdk_binaries.sh
-git commit -m "Update GDK to ${NAME}" -S
+git add ./tools/fetch_gdk_binaries.sh
+git commit -m "Update GDK to ${TAGNAME}" -S
+rm -f IOS_SHA256SUMS.asc IOS_SHA256SUMS
