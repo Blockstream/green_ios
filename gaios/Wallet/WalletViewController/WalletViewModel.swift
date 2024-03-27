13be78388f3c17f1e769bff6d45fdab0d6799472
Mauro Olivo <olivomauro@gmail.com>
2024-03-27 12:03:19 +0100

    ui: fix 0 balance denom to BTC if no accounts

--- gaios/Wallet/WalletViewController/WalletViewModel.swift
+++ gaios/Wallet/WalletViewController/WalletViewModel.swift
@@ -66,7 +66,7 @@ class WalletViewModel {
 
     func getAssetId() -> String {
         let lSubs: [WalletItem] = subaccounts.filter{ $0.gdkNetwork.liquid == true }
-        if lSubs.count == subaccounts.count {
+        if lSubs.count == subaccounts.count && lSubs.count > 0 {
             return lSubs.first?.gdkNetwork.mainnet ?? true ? Balance.lbtc : Balance.ltest
         } else {
             return "btc"
