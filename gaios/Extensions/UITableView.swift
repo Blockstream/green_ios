1aaafba8f2866b876133c776b734a33d8fd2d35f
Mauro Olivo <olivomauro@gmail.com>
2024-09-30 13:26:36 +0200

    Improve wallet refresh

--- gaios/Extensions/UITableView.swift
+++ gaios/Extensions/UITableView.swift
@@ -4,3 +4,17 @@ extension UITableView {
         UIView.animate(withDuration: 0, animations: reloadData) { _ in completion() }
     }
 }
+extension UITableView {
+    func beginRefreshing() {
+        guard let refreshControl = refreshControl, !refreshControl.isRefreshing else {
+            return
+        }
+        refreshControl.beginRefreshing()
+        refreshControl.sendActions(for: .valueChanged)
+        let contentOffset = CGPoint(x: 0, y: -refreshControl.frame.height)
+        setContentOffset(contentOffset, animated: true)
+    }
+    func endRefreshing() {
+        refreshControl?.endRefreshing()
+    }
+}
--- gaios/Wallet/WalletViewController/WalletViewController.swift
+++ gaios/Wallet/WalletViewController/WalletViewController.swift
@@ -1056,6 +1056,7 @@ extension WalletViewController: DialogGroupListViewControllerDelegate {
                         present(vc, animated: false, completion: nil)
                     }
                 case .refresh:
+                    tableView.beginRefreshing()
                     reload(discovery: true)
                 case .archive:
                     showArchived()
