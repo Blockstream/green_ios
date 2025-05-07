a9811e2b7c1964dcf0e8c8f0b7f4506d74f6e6a8
Domenico Gabriele <domenico@blockstream.com>
2025-05-07 21:57:15 +0200

    buy: show quoted amount in btc

--- gaios/BuyBTC/BuyBTCViewController.swift
+++ gaios/BuyBTC/BuyBTCViewController.swift
@@ -179,7 +179,7 @@ class BuyBTCViewController: KeyboardViewController {
             let initials = viewModel.getInitials(from: pName)
             lblIconProvider.text = initials
             bgIconProvider.backgroundColor = viewModel.colorFromProviderName(pName)
-            lblDenom.text = "≈ \(quotes[selectedIndex].btc() ?? "")"
+            lblDenom.text = "\(quotes[selectedIndex].destinationAmount) BTC"
         }
         btnAccount.setTitle(viewModel.account.localizedName, for: .normal)
         setBackupCard()
