87761159cfa7b7f0b7a614643d8e2bbad7dcd8d2
Mauro Olivo <olivomauro@gmail.com>
2025-06-16 14:08:05 +0200

    Fix images for consistency

--- gaios/HWFlow/Wait/JadeWaitViewModel.swift
+++ gaios/HWFlow/Wait/JadeWaitViewModel.swift
@@ -21,17 +21,17 @@ class JadeWaitViewModel {
                          titleStep: "id_step".localized.uppercased() + " 1",
                          title: "id_power_on_jade".localized,
                          hint: "Hold the button on the bottom of Jade until it boots up".localized,
-                         placeholderName: "il_jade_select_v2"),
+                         placeholderName: "il_jade_ph_1_power_on"),
             JadeWaitStep(riveModel: RiveModel.animationFrontBtn,
                          titleStep: "id_step".localized.uppercased() + " 2",
                          title: "id_follow_the_instructions_on_jade".localized,
                          hint: "id_select_initalize_to_create_a".localized,
-                         placeholderName: nil),
+                         placeholderName: "il_jade_ph_2_follow_instructions"),
             JadeWaitStep(riveModel: RiveModel.animationSideBtns,
                          titleStep: "id_step".localized.uppercased() + " 3",
                          title: "id_connect_with_bluetooth".localized,
                          hint: "id_choose_bluetooth_connection_on".localized,
-                         placeholderName: nil)
+                         placeholderName: "il_jade_ph_3_connect_ble")
         ]
     }
 }
