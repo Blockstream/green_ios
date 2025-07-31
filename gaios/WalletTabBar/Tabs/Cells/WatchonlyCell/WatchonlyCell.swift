8a61e67a217e3e47df388695c407a55798613816
Mauro Olivo <olivomauro@gmail.com>
2025-07-31 08:36:27 +0200

    Fix watch-only security tab

--- gaios/WalletTabBar/Tabs/Cells/WatchonlyCell/WatchonlyCell.swift
+++ gaios/WalletTabBar/Tabs/Cells/WatchonlyCell/WatchonlyCell.swift
@@ -6,21 +6,19 @@ class WatchonlyCell: UITableViewCell {
     @IBOutlet weak var icon: UIImageView!
     @IBOutlet weak var lblTitle: UILabel!
     @IBOutlet weak var lblHint: UILabel!
+    @IBOutlet weak var btnLearnMore: UIButton!
     var onLearnMore: (() -> Void)?
 
     class var identifier: String { return String(describing: self) }
 
     override func prepareForReuse() {}
 
-    let strWo = "In a watch-only wallet, you don’t have access to the private keys, so you can’t send funds - you can only view your balance and transaction history. Learn more about watch-only.".localized
-    let strSend = "you can’t send funds".localized
-    let strMore = "id_learn_more".localized
-
     override func awakeFromNib() {
         super.awakeFromNib()
         lblTitle.setStyle(.title)
         lblTitle.font = UIFont.systemFont(ofSize: 28, weight: .medium)
         lblHint.setStyle(.txtCard)
+        btnLearnMore.setStyle(.inline)
     }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
@@ -28,37 +26,10 @@ class WatchonlyCell: UITableViewCell {
     func configure(onLearnMore: (() -> Void)?) {
         self.onLearnMore = onLearnMore
         lblTitle.text = "id_watchonly".localized
-        setStyle()
+        lblHint.text = "id_in_a_watchonly_wallet_your".localized
+        btnLearnMore.setTitle("id_learn_more".localized, for: .normal)
     }
-    func setStyle() {
-
-        let pStyle = NSMutableParagraphStyle()
-        pStyle.lineSpacing = 2.0
-        let linkAttr: [NSAttributedString.Key: Any] = [
-            .foregroundColor: UIColor.gAccent(),
-            NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
-        ]
-        let boldAttr: [NSAttributedString.Key: Any] = [
-            .foregroundColor: UIColor.white,
-            .font: UIFont.systemFont(ofSize: 16, weight: .medium)
-        ]
-        let attrStr = NSMutableAttributedString(string: strWo)
-        attrStr.addAttribute (
-            NSAttributedString.Key.paragraphStyle,
-            value: pStyle,
-            range: NSRange(location: 0, length: attrStr.length))
-        attrStr.addAttribute(NSAttributedString.Key.font, value: UIFont.systemFont(ofSize: 16, weight: .regular), range: NSRange(location: 0, length: attrStr.length))
-        attrStr.setAttributes(boldAttr, for: strSend)
-        attrStr.setAttributes(linkAttr, for: strMore)
-        lblHint.attributedText = attrStr
-        lblHint.isUserInteractionEnabled = true
-        lblHint.lineBreakMode = .byWordWrapping
-        lblHint.textAlignment = .center
-        let tapGesture = UITapGestureRecognizer.init(target: self, action: #selector(onTap(_:)))
-        tapGesture.numberOfTouchesRequired = 1
-        lblHint.addGestureRecognizer(tapGesture)
-    }
-    @objc func onTap(_ sender: Any?) {
+    @IBAction func btnLearnMore(_ sender: Any) {
         onLearnMore?()
     }
 }
