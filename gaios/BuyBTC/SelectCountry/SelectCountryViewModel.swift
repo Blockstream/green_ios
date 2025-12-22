53a5ee45168c26cb55a33d288c75a83ef1090c8a
Jonikorjk <zenaserdykov2004@gmail.com>
2025-12-22 12:09:45 +0200

    Provide search by country name and code

--- gaios/BuyBTC/SelectCountry/SelectCountryViewModel.swift
+++ gaios/BuyBTC/SelectCountry/SelectCountryViewModel.swift
@@ -2,10 +2,22 @@ import Foundation
 import UIKit
 
 class SelectCountryViewModel {
-
     var title = "Billing"
     var hint = "Please select your correct billing location to complete the checkout successfully."
-    var countries = Country.allMeld()
-    init() {
+
+    private var countries = Country.allMeld()
+    var filteredCountries = Country.allMeld()
+
+    func searchCountries(_ searchText: String) {
+        let query = searchText
+            .lowercased()
+            .replacingOccurrences(of: " ", with: "")
+            .folding(options: .diacriticInsensitive, locale: .current);
+
+        guard !query.isEmpty else { filteredCountries = Country.allMeld(); return }
+
+        filteredCountries = countries.filter {
+            $0.code.lowercased().contains(query) || $0.name.lowercased().contains(query)
+        }
     }
 }
